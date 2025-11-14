68c39b52ad6b2ee1e54331c776ee51b9d3f442d8
RamzitheStudent <12554@holbertonstudents.com>
2025-11-14 06:55:33 +0000

    mn

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
-1 #!/usr/bin/python3
-2 str1 = "Welcome to"
-3 str2 = "Holberton School!"
-4 print(str1 + " " + str2)
-5 <MUST NOT BE BLANK>
-
+#!/usr/bin/python3
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1 + " " + str2)
+<this is blank>
