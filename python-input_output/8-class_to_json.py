2da3f45ee776d1cffe07c4713144525719025e09
RamzitheStudent <12554@holbertonstudents.com>
2025-11-24 11:13:33 +0000

    mr

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
 def class_to_json(obj):
     """Returns the dictionary description for JSON serialization"""
-    return obj.__dict__
+    result = {}
+
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            result[key] = value
+
+    return result
+
