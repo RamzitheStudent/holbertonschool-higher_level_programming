18382ae661207f5eb55872aad821786056d76746
RamzitheStudent <12554@holbertonstudents.com>
2025-11-24 10:15:24 +0000

    dr

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """nothing"""
 def write_file(filename="", text=""):
-"""cavab budur"""
+    """cavab budur"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
