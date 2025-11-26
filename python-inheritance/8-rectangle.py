f6609fc292c04519069c9727338e23b027123bf5
RamzitheStudent <12554@holbertonstudents.com>
2025-11-26 10:06:09 +0000

    gm

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class inheriting from BaseGeometry."""
+
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class with private width and height."""
+
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
