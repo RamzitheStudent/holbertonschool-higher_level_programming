52ba8f8d4a77d0fa158bbc9b34e7c74e323cb4c6
RamzitheStudent <12554@holbertonstudents.com>
2025-11-26 10:29:42 +0000

    gm

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class inheriting from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class with private width and height."""
+
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle.
+
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation: [Rectangle] <width>/<height>."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
