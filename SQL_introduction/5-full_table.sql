1dc9a5c413aa4ecd315e02f49bb74f12fe222605
RamzitheStudent <12554@holbertonstudents.com>
2025-11-28 08:37:47 +0000

    gm

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,15 +1,2 @@
 -- comment
-mysql -u root -p "$1" -e "
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM information_schema.columns
-WHERE table_schema = '$1'
-  AND table_name = 'first_table'
-ORDER BY ORDINAL_POSITION;
-"
-
+SHOW CREATE TABLE first_table;
