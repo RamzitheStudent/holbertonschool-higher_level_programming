eb340eda9f86d2084646a5df8b3dc98e684ead00
RamzitheStudent <12554@holbertonstudents.com>
2025-11-28 08:29:12 +0000

    mn

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,15 @@
 -- comment
-SELECT COLUMN_NAME, COLUMN_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';"
+mysql -u root -p "$1" -e "
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM information_schema.columns
+WHERE table_schema = '$1'
+  AND table_name = 'first_table'
+ORDER BY ORDINAL_POSITION;
+"
+
