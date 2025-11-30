427df1291ad43a503818ba8bbd0fe6d693c88a73
RamzitheStudents <12554@holbertonstudents.com>
2025-11-30 09:23:15 +0000

    gm

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+SELECT
+    tg.name AS genre,
+    COUNT(tsg.show_id) AS number_of_shows
+FROM
+    tv_genres AS tg
+INNER JOIN
+    tv_show_genres AS tsg ON tg.id = tsg.genre_id
+GROUP BY
+    tg.name
+ORDER BY
+    number_of_shows DESC;
