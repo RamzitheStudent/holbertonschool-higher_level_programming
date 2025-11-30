0f63185d848a894f8c338ebd2db12347dec8a501
RamzitheStudents <12554@holbertonstudents.com>
2025-11-30 09:06:21 +0000

    gm

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,14 @@
+-- comment
+SELECT
+    id,
+    name
+FROM
+    cities
+WHERE
+    state_id = (
+        SELECT id
+        FROM states
+        WHERE name = 'California'
+    )
+ORDER BY
+    id ASC;
