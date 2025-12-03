746c5beefe5f3d582b131aab7c3dde0bf5bed7c9
RamzitheStudent <12554@holbertonstudents.com>
2025-12-03 07:43:04 +0000

    gm

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib and prints the body as a string"""
+"""Fetches https://intranet.hbtn.io/status using requests"""
 
-from urllib import request
-import sys
+import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-
-    with request.urlopen(url) as response:
-        body = response.read().decode("utf-8")
+    response = requests.get(url)
+    body = response.text  # decode automatically to str
 
     print("Body response:")
     print("\t- type: {}".format(type(body)))
