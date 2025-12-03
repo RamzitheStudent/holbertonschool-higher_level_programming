e345aeadac041e1407ce31e26c3d2631d96f6626
RamzitheStudent <12554@holbertonstudents.com>
2025-12-03 07:45:07 +0000

    gm

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetches a URL and displays the value of X-Request-Id in the response headers"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
