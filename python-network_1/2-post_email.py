c758d5ea6a13365c2f3394c3e9198c79053d6dcd
RamzitheStudent <12554@holbertonstudents.com>
2025-12-03 07:21:20 +0000

    gm

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Sends a POST request to a URL with an email and displays the response body"""
+
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Prepare POST data
+    data = parse.urlencode({"email": email}).encode("utf-8")
+
+    # Send POST request
+    req = request.Request(url, data=data, method="POST")
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode("utf-8"))
