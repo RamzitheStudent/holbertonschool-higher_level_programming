74b3e0cea73a7bee57fe2031311d88670f36d563
RamzitheStudent <12554@holbertonstudents.com>
2025-12-05 08:39:18 +0000

    gm

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_file = os.path.join(os.path.dirname(__file__), 'items.json')
+
+    # Read JSON data
+    try:
+        with open(items_file, 'r') as file:
+            data = json.load(file)
+        items_list = data.get("items", [])
+    except Exception as e:
+        print(f"Error reading JSON file: {e}")
+        items_list = []
+
+    # Render the template with the list of items
+    return render_template('items.html', items=items_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
